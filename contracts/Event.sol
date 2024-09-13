df2504efb15cd4a8d00720f07dc402f202d9fd3d
Abolare Roheemah Ayomide <roheemah@Abolares-MacBook-Air.local>
2024-09-13 18:49:26 +0100

    removed comment

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract Event {
     uint eventCount;
     address owner;
