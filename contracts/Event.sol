518f8b249285f967b0531e36b3abc14af484b2d2
Abolare Roheemah Ayomide <roheemah@Abolares-MacBook-Air.local>
2024-09-13 17:10:10 +0100

    adding more checks

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -8,16 +8,21 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Event {
     uint eventCount;
+    address owner;
     struct EventStruct {
        string name;
        uint eventId;
        uint startAt;
-       uint EndAt;
+       uint endAt;
        bool isCompleted;
        address eventNFT;
        uint nftId;
        uint noOfRegisteredAttendees; 
        address[] registeredAttendees;
+    }
+
+    constructor (address _owner) {
+        owner = _owner;
     }
 
     EventStruct[] allEvents;
@@ -28,7 +33,9 @@ contract Event {
 
     function createEvent(address _nftAddress, string memory _name, uint _nftId, uint _startAt, uint _endAt) external {
         require(msg.sender != address(0), "invalid caller");
+        require(msg.sender == owner, "not authorized");
         require(_nftAddress != address(0), "invalid nft address");
+        require(_startAt >= block.timestamp, "invalid start time");
 
         uint newCount = eventCount + 1;
         EventStruct storage newEvent = events[newCount];
@@ -37,7 +44,7 @@ contract Event {
         newEvent.eventNFT = _nftAddress;
         newEvent.nftId = _nftId;
         newEvent.startAt = _startAt;
-        newEvent.EndAt = _endAt;
+        newEvent.endAt = _endAt;
 
         allEvents.push(newEvent);
         eventCount = newCount;
@@ -47,13 +54,23 @@ contract Event {
 
     function attendeeReg(uint _eventId, address _applicant) external {
         require(msg.sender != address(0), "invalid caller");
+        require(_applicant != address(0), "invalid applicant address");
         EventStruct storage newEvent = events[_eventId];
         require(newEvent.eventId != 0, "invalid event id");
+        require(newEvent.endAt > block.timestamp, "event has ended");
+        require(newEvent.startAt <= block.timestamp, "event hasnt started");
         require(IERC721(newEvent.eventNFT).balanceOf(_applicant) > 0, "required NFT not found");
         
         newEvent.registeredAttendees.push(_applicant);
         newEvent.noOfRegisteredAttendees++;
 
         emit ApplicantRegistered(_eventId, _applicant);
+    }
+
+    function mintEventNFT(address _to, address _nftAddress, uint256 _tokenId) internal {
+        require(msg.sender == owner, "not authorized");
+        IERC721 nft = IERC721(_nftAddress);
+        require(nft.ownerOf(_tokenId) == address(this), "Contract doesn't own the NFT");
+        nft.safeTransferFrom(address(this), _to, _tokenId);
     }
 } // 0x207De8361C16b18CA60b41F7D68A45B99E11d7A2
