3c06c72a53b9e6118e08fbeabc437b164e9d5fdd
Abolare Roheemah Ayomide <roheemah@Abolares-MacBook-Air.local>
2024-09-13 16:04:08 +0100

    function to register attendees

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
@@ -14,13 +16,15 @@ contract Event {
        bool isCompleted;
        address eventNFT;
        uint nftId;
-       uint registeredAttendees; 
+       uint noOfRegisteredAttendees; 
+       address[] registeredAttendees;
     }
 
     EventStruct[] allEvents;
     mapping (uint => EventStruct) events;
 
     event EventCreated(string name, uint startAt, uint endAt);
+    event ApplicantRegistered(uint indexed eventId, address indexed applicant);
 
     function createEvent(address _nftAddress, string memory _name, uint _nftId, uint _startAt, uint _endAt) external {
         require(msg.sender != address(0), "invalid caller");
@@ -30,6 +34,7 @@ contract Event {
         EventStruct storage newEvent = events[newCount];
         newEvent.name = _name;
         newEvent.eventId = newCount;
+        newEvent.eventNFT = _nftAddress;
         newEvent.nftId = _nftId;
         newEvent.startAt = _startAt;
         newEvent.EndAt = _endAt;
@@ -40,11 +45,15 @@ contract Event {
         emit EventCreated(_name, _startAt, _endAt);
     }
 
-    function attendeeReg(uint _eventId) external {
+    function attendeeReg(uint _eventId, address _applicant) external {
         require(msg.sender != address(0), "invalid caller");
         EventStruct storage newEvent = events[_eventId];
         require(newEvent.eventId != 0, "invalid event id");
+        require(IERC721(newEvent.eventNFT).balanceOf(_applicant) > 0, "required NFT not found");
         
-        
+        newEvent.registeredAttendees.push(_applicant);
+        newEvent.noOfRegisteredAttendees++;
+
+        emit ApplicantRegistered(_eventId, _applicant);
     }
 } // 0x207De8361C16b18CA60b41F7D68A45B99E11d7A2
